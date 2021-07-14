f2fd182c2334d69221af9595612c5a5bfd390673
rbciambarella <rbciambarella@gmail.com>
2021-07-14 15:29:23 -0300

    Aula React na Gana Academy Hiribg Coders II

---
+++ routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Repositories from './pages/Repositories';
+import Home from './pages/Home';
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/repositories' component={Repositories} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+// estrutura base arquivo de rotas - router
